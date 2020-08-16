af4c4c834d4e0af34d22842395e09d1c37cfb3fe
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-16 11:26:45 -0500

    Create load.R

---
+++ R/load.R
@@ -0,0 +1,11 @@
+loadR <- function() { 
+  library(tidyverse)
+  library(janitor)
+  library(glue)
+  library(widyr)
+  library(modelr)
+  library(broom)
+  library(readxl)
+  library(hms)
+  library(lubridate)
+} 
