40280629c6d75c58b5cd7c8d70bb9af5991233eb
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-08-21 18:12:35 -0500

    Update web_browse.R

--- R/web_browse.R
+++ R/web_browse.R
@@ -1,5 +1,5 @@
 web_browse <- function(x) { 
-  file = stringr::str_c(tempfile(deparse(substitute(COPD)),
+  file = stringr::str_c(tempfile(deparse(substitute(x)),
                         fileext = ".csv"))
   readr::write_csv(x, path = file)
   browseURL(file) 
