dc58f0b072a3d5819b604d9132b9a853b1064d8a
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-09-26 13:04:13 -0500

    Update lock_binding.R

--- R/lock_binding.R
+++ R/lock_binding.R
@@ -1,5 +1,6 @@
-`:=` <- function(lhs, rhs) { 
+`:=` <- function(lhs, rhs) {
   var <- deparse(substitute(lhs))
-  ifelse(exists(var, parent.frame(), inherits = FALSE), stop("Variable `", var, "` is already defined.", call. = FALSE),
+  ifelse(exists(var, parent.frame(), inherits = FALSE),
+         stop("Variable `", var, "` is already defined.", call. = FALSE),
          assign(var, rhs, parent.frame()))
-} 
+}
