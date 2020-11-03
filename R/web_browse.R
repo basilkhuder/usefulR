6e369fbfef6a79c7022b0e67276b29d397428e7c
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-11-03 11:19:22 -0600

    Update web_browse.R

--- R/web_browse.R
+++ R/web_browse.R
@@ -1,6 +1,26 @@
-web_browse <- function(x) { 
+web_browse <- function(x, ...) { 
+  UseMethod("web_browse")
+} 
+
+web_browse.data.frame <- function(x, ...){ 
+  
   file = stringr::str_c(tempfile(deparse(substitute(x)),
-                        fileext = ".csv"))
+                                 fileext = ".csv"))
   readr::write_csv(x, path = file)
   browseURL(file) 
-} 
+
+}
+
+web_browse.gg <- function(x, 
+                          width = 20, 
+                          height = 20, 
+                          ...){ 
+  file = stringr::str_c(tempfile(deparse(substitute(x)),
+                                 fileext = ".png"))
+  ggplot2::ggsave(plot = x, filename = file, width = width, height = height)
+  browseURL(file)
+ }
+
+
+ 
+
