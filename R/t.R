e3ea237a16a6c0629d1fcf0e583792a69921b587
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-11-11 00:39:09 -0600

    Update t.R

--- R/t.R
+++ R/t.R
@@ -19,7 +19,7 @@ t.tbl_df <-  function(x,
   df_transpose <- data.table::transpose(x)
   dimnames(df_transpose) <- list(colnames(x), rownames(x))
   df_transpose <- tibble::rownames_to_column(df_transpose)
-  df_parse <- paste0(df_types[[return_type]], "(transpose_df)")
+  df_parse <- paste0(df_types[[return_type]], "(df_transpose)")
   df_transpose <- eval(parse(text = df_parse))
   colnames(df_transpose) <- as.character(df_transpose[1,])
   df_transpose <- df_transpose[2:nrow(df_transpose),]
