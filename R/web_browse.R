6341ad54cea2aa9a834f74aaee388a2f944f2e35
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-11-11 23:11:48 -0600

    Update web_browse.R

--- R/web_browse.R
+++ R/web_browse.R
@@ -18,9 +18,9 @@ web_browse.gg <- function(x,
   if (isTRUE(save)) {
     if (is.null(file_name)) {
       file = paste0(gsub("-", "", Sys.Date()), "_image_output.png")
-    } else { 
+    } else {
       file = file_name
-      }
+    }
     
     ggplot2::ggsave(
       plot = x,
@@ -39,5 +39,36 @@ web_browse.gg <- function(x,
     )
   }
   
+  browseURL(file)
+}
+
+
+web_browse.pheatmap <- function(x,
+                                width = 20,
+                                height = 20,
+                                save = FALSE,
+                                file_name = NULL,
+                                ...) {
+  file = tempfile(paste0(deparse(substitute(x)), "_"),
+                  fileext = ".png")
+  
+  if (isTRUE(save)) {
+    if (is.null(file_name)) {
+      file = paste0(gsub("-", "", Sys.Date()), "_image_output.png")
+    } else {
+      file = file_name
+    }
+  }
+  
+  png(
+    file,
+    width = width,
+    height = height,
+    units = "in",
+    res = 300
+  )
+  grid::grid.newpage()
+  grid::grid.draw(x$gtable)
+  dev.off()
   browseURL(file)
 }
