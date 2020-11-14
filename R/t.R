33a9cf3b4f1d320d5b1d1f86b0ce04f1605ecdf5
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-11-14 15:13:35 -0600

    Update t.R

--- R/t.R
+++ R/t.R
@@ -2,36 +2,40 @@ t <- function(x, ...) {
   UseMethod("t")
 }
 
-t.tbl_df <-  function(x,
-                      return_type = "tibble",
-                      col_as_rnames = FALSE,
+t.tbl_df <-  function(x, 
+                      col_types = NULL,
                       ...) {
-  df_types <-
-    setNames(
-      c(
-        "tibble::as_tibble",
-        "as.data.frame",
-        "data.table::as.data.table"
-      ),
-      c("tibble", "data.frame", "data.table")
-    )
   
   df_transpose <- data.table::transpose(x)
   dimnames(df_transpose) <- list(colnames(x), rownames(x))
   df_transpose <- tibble::rownames_to_column(df_transpose)
-  df_parse <- paste0(df_types[[return_type]], "(df_transpose)")
-  df_transpose <- eval(parse(text = df_parse))
-  colnames(df_transpose) <- as.character(df_transpose[1,])
-  df_transpose <- df_transpose[2:nrow(df_transpose),]
+  df_transpose <- tibble::as_tibble(df_transpose)
+  colnames(df_transpose) <- as.character(df_transpose[1, ])
+  df_transpose <- df_transpose[2:nrow(df_transpose), ]
   
-  if (isTRUE(col_as_rnames)) {
-    rownames(df_transpose) <- df_transpose[[1]]
-    df_transpose <- df_transpose[-1]
-  }
-  
+  file <- tempfile("df_transpose", fileext = ".txt")
+  readr::write_tsv(df_transpose, file)
+  df_transpose <- suppressWarnings(readr::read_tsv(file, col_types = cols()))
   return(df_transpose)
 }
 
 t.data.table <- function(x, ...) {
   return(data.table::transpose(x, ...))
+}
+
+as_tibble.matrix <- function(matrix,
+                             rn_as_col = TRUE,
+                             rn = "rn") {
+  mat_attr <- purrr::map(1:2, ~ dimnames(matrix)[[.x]])
+  dimnames(matrix) <- NULL
+  df <- as.data.frame(matrix)
+  
+  if (isTRUE(rn_as_col)) {
+    colnames(df) <- mat_attr[[2]]
+    df <-
+      dplyr::mutate(df,!!as.name(rn) := mat_attr[[1]], .before = everything())
+  } else {
+    dimnames(df) <- list(mat_attr[[1]], mat_attr[[2]])
+  }
+  return(as_tibble(df))
 }
