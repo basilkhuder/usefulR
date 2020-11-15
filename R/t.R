75722cc159b9eb813643a18376156ffed8b272e3
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-11-14 18:24:26 -0600

    Update t.R

--- R/t.R
+++ R/t.R
@@ -5,7 +5,7 @@ t <- function(x, ...) {
 t.tbl_df <-  function(x, 
                       ...) {
   
-  df_transpose <- data.table::transpose(x)
+  df_transpose <- data.table::transpose(x, ...)
   dimnames(df_transpose) <- list(colnames(x), rownames(x))
   df_transpose <- tibble::rownames_to_column(df_transpose)
   df_transpose <- tibble::as_tibble(df_transpose)
