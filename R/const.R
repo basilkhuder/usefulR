15ad469beea5c3cd03d94f850bc06f271e84dba5
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-11-27 15:24:32 -0600

    Update const.R

--- R/const.R
+++ R/const.R
@@ -3,7 +3,7 @@
 `%=%` <- function(lhs, rhs){
   let <- deparse(substitute(lhs))
   if (exists(let, parent.frame(), inherits = FALSE)) { 
-    stop(glue::glue("The Variable you are trying to create, {let}, already exists."))
+    stop (glue::glue("The Variable you are trying to create, {let}, already exists."))
     } else { 
       assign(let, rhs, parent.frame())
     } 
