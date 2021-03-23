0864c01b2c54fc224b0b095460c360b5156f00e9
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-03-23 09:46:50 -0500

    Update const.R

--- R/const.R
+++ R/const.R
@@ -1,6 +1,6 @@
 #Creates a constant variable type that cannot be changed. Similiar to Javascript's const.
 
-`%=%` <- function(lhs, rhs){
+`%<-%` <- function(lhs, rhs){
   let <- deparse(substitute(lhs))
   if (exists(let, parent.frame(), inherits = FALSE)) { 
     stop (glue::glue("The Variable you are trying to create, {let}, already exists."))
