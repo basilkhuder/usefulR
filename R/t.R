82defe9e1029172f4b651a3aa58fc77b11bd5df2
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2021-12-24 21:44:51 -0700

    Update t.R
    Fixed bug in as_tibble.matrix() where dimension names weren't being added properly. Made rn_as_col FALSE as default.

--- R/t.R
+++ R/t.R
@@ -1,16 +1,16 @@
-t.tbl_df <-  function(x, 
+t.tbl_df <-  function(x,
                       ...) {
-  
   df_transpose <- data.table::transpose(x, ...)
   dimnames(df_transpose) <- list(colnames(x), rownames(x))
   df_transpose <- tibble::rownames_to_column(df_transpose)
   df_transpose <- tibble::as_tibble(df_transpose)
-  colnames(df_transpose) <- as.character(df_transpose[1, ])
-  df_transpose <- df_transpose[2:nrow(df_transpose), ]
+  colnames(df_transpose) <- as.character(df_transpose[1,])
+  df_transpose <- df_transpose[2:nrow(df_transpose),]
   
   file <- tempfile("df_transpose", fileext = ".txt")
   readr::write_tsv(df_transpose, file)
-  df_transpose <- suppressWarnings(readr::read_tsv(file, col_types = cols()))
+  df_transpose <-
+    suppressWarnings(readr::read_tsv(file, col_types = cols()))
   return(df_transpose)
 }
 
@@ -18,19 +18,26 @@ t.data.table <- function(x, ...) {
   return(data.table::transpose(x, ...))
 }
 
-as_tibble.matrix <- function(matrix,
-                             rn_as_col = TRUE,
+as_tibble.matrix <- function(mat,
+                             rn_as_col = FALSE,
                              rn = "rn") {
-  mat_attr <- purrr::map(1:2, ~ dimnames(matrix)[[.x]])
-  dimnames(matrix) <- NULL
-  df <- as.data.frame(matrix)
+  if (is.null(dimnames(mat)[[2]])) {
+    cols <- dim(mat)[[2]]
+    cols <- seq(cols)
+    mat_attr <- cols
+  } else {
+    mat_attr <- dimnames(mat)[[2]]
+  }
+  
+  dimnames(mat) <- NULL
+  df <- as.data.frame(mat)
   
   if (rn_as_col) {
-    colnames(df) <- mat_attr[[2]]
+    colnames(df) <- mat_attr
     df <-
-      dplyr::mutate(df,!!as.name(rn) := mat_attr[[1]], .before = everything())
+      dplyr::mutate(df, !!as.name(rn) := mat_attr[[1]], .before = everything())
   } else {
-    dimnames(df) <- list(mat_attr[[1]], mat_attr[[2]])
+    colnames(df) <- mat_attr
   }
   return(as_tibble(df))
 }
